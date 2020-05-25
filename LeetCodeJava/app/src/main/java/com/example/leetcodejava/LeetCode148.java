3f9fa42be4033c4aa2265a90a2b2917b449580a1
reborn <343633674@qq.com>
2020-05-25 18:55:50 +0800

    commit

---
+++ LeetCodeJava/app/src/main/java/com/example/leetcodejava/LeetCode148.java
@@ -0,0 +1,33 @@
+package com.example.leetcodejava;
+
+public class LeetCode148 {
+    /**
+     * 148、排序链表
+     * 在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序。
+     *
+     * 示例 1:
+     * 输入: 4->2->1->3
+     * 输出: 1->2->3->4
+     *
+     * 示例 2:
+     * 输入: -1->5->3->4->0
+     * 输出: -1->0->3->4->5
+     */
+
+    /**
+     * Definition for singly-linked list.
+     */
+    public class ListNode {
+        int val;
+        LeetCode147.ListNode next;
+        ListNode(int x) { val = x;}
+    }
+
+    public ListNode sortList(ListNode head) {
+
+
+
+    }
+
+
+}
