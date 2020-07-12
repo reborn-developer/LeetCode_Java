8839052929b117ba2514799a0025a828b5d85dc1
reborn <343633674@qq.com>
2020-07-12 21:41:23 +0800

    commit

--- LeetCodeJava/app/src/main/java/com/example/leetcodejava/LeetCode315.java
+++ LeetCodeJava/app/src/main/java/com/example/leetcodejava/LeetCode315.java
@@ -1,6 +1,11 @@
 package com.example.leetcodejava;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class LeetCode315 {
     /**
@@ -20,7 +25,64 @@ public class LeetCode315 {
      * 6 的右侧有 1 个更小的元素 (1).
      * 1 的右侧有 0 个更小的元素.
      */
-    public List<Integer> countSmaller(int[] nums) {
 
+    private int[] c;
+    private int[] a;
+
+    public List<Integer> countSmaller(int[] nums) {
+        List<Integer> resultList = new ArrayList<Integer>();
+        discretization(nums);
+        init(nums.length + 5);
+        for (int i = nums.length - 1; i >= 0; --i) {
+            int id = getId(nums[i]);
+            resultList.add(query(id - 1));
+            update(id);
+        }
+        Collections.reverse(resultList);
+        return resultList;
+    }
+
+    private void init(int length) {
+        c = new int[length];
+        Arrays.fill(c, 0);
+    }
+
+    private int lowBit(int x) {
+        return x & (-x);
+    }
+
+    private void update(int pos) {
+        while (pos < c.length) {
+            c[pos] += 1;
+            pos += lowBit(pos);
+        }
+    }
+
+    private int query(int pos) {
+        int ret = 0;
+        while (pos > 0) {
+            ret += c[pos];
+            pos -= lowBit(pos);
+        }
+
+        return ret;
+    }
+
+    private void discretization(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        int size = set.size();
+        a = new int[size];
+        int index = 0;
+        for (int num : set) {
+            a[index++] = num;
+        }
+        Arrays.sort(a);
+    }
+
+    private int getId(int x) {
+        return Arrays.binarySearch(a, x) + 1;
     }
 }
