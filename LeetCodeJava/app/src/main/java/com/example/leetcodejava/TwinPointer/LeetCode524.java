3ce802585a9270bfc2f168a9d85fa1804415f5f2
reborn <343633674@qq.com>
2020-07-17 19:06:49 +0800

    commit

--- LeetCodeJava/app/src/main/java/com/example/leetcodejava/TwinPointer/LeetCode524.java
+++ LeetCodeJava/app/src/main/java/com/example/leetcodejava/TwinPointer/LeetCode524.java
@@ -1,4 +1,4 @@
-package com.example.leetcodejava;
+package com.example.leetcodejava.TwinPointer;
 
 import java.util.Collections;
 import java.util.Comparator;
@@ -7,7 +7,7 @@ import java.util.List;
 public class LeetCode524 {
 
     /**
-     * LeetCode524.通过删除字母匹配到字典里最长单词
+     * LeetCode524.通过删除字母匹配到字典里最长单词    Medium
      * 给定一个字符串和一个字符串字典，找到字典里面最长的字符串，该字符串可以通过删除给定字符串的某些字符来得到。
      * 如果答案不止一个，返回长度最长且字典顺序最小的字符串。如果答案不存在，则返回空字符串。
      *
@@ -44,16 +44,16 @@ public class LeetCode524 {
     * */
 
     public String findLongestWord(String s, List<String> d) {
-        String str="";
+        String result="";
         for(String sstr:d){
             for(int i=0,j=0;i<s.length()&&j<sstr.length();i++){
                 if(s.charAt(i)==sstr.charAt(j)) j++;
                 if(j==sstr.length()){
-                    if(sstr.length()>str.length()||(sstr.length()==str.length()&&str.compareTo(sstr)>0))  str=sstr;
+                    if(sstr.length()>result.length()||(sstr.length()==result.length()&&result.compareTo(sstr)>0))  result=sstr;
                 }
             }
         }
-        return str;
+        return result;
     }
 
     /**
@@ -76,19 +76,22 @@ public class LeetCode524 {
                 return s2.length() != s1.length() ? s2.length() - s1.length() : s1.compareTo(s2);
             }
         });
-        for (String str: d) {
-            if (isSubsequence(str, s))
-                return str;
+        for (String t: d) {
+            if (isSubsequence(s, t))
+                return t;
         }
         return "";
     }
 
     public boolean isSubsequence(String s, String t) {
-        int j = 0;
-        for (int i = 0; j < s.length() && i < t.length(); i++)
-            if (s.charAt(j) == t.charAt(i))
+        int i = 0, j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
                 j++;
-        return j == s.length();
+            }
+            i++;
+        }
+        return j == t.length();
     }
 
     /**
