9a417078b8298b0e3bbabbb238bfa65c21686a72
reborn <343633674@qq.com>
2020-07-17 16:20:48 +0800

    LeetCode141. 环形链表

---
+++ LeetCodeJava/app/src/main/java/com/example/leetcodejava/TwinPointer/LeetCode141.java
@@ -0,0 +1,60 @@
+package com.example.leetcodejava.TwinPointer;
+
+public class LeetCode141 {
+    /**
+     * LeetCode141. 环形链表
+     *
+     * 给定一个链表，判断链表中是否有环。
+     *
+     * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+     *
+     * 示例 1：
+     *
+     * 输入：head = [3,2,0,-4], pos = 1
+     * 输出：true
+     * 解释：链表中有一个环，其尾部连接到第二个节点。
+     *
+     * 示例 2：
+     *
+     * 输入：head = [1,2], pos = 0
+     * 输出：true
+     * 解释：链表中有一个环，其尾部连接到第一个节点。
+     *
+     * 示例 3：
+     *
+     * 输入：head = [1], pos = -1
+     * 输出：false
+     * 解释：链表中没有环。
+     */
+
+    /**
+     * 方法：双指针
+     * 使用双指针，一个指针每次移动一个节点，一个指针每次移动两个节点，如果存在环，那么这两个指针一定会相遇。
+     */
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+    public boolean hasCycle(ListNode head) {
+
+        if (head == null) {
+            return false;
+        }
+        ListNode p1 = head;
+        ListNode p2 = head.next;
+
+        while (p1 != null && p2 != null && p2.next != null) {
+            if (p1 == p2) {
+                return true;
+            }
+            p1 = p1.next;
+            p2 = p2.next.next;
+        }
+        return false;
+    }
+
+}
