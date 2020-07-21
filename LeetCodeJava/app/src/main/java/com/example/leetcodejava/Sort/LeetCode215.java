9b8da23fd3c32fea704bb423f9a0a81f3e8c5fdb
reborn <343633674@qq.com>
2020-07-21 16:48:17 +0800

    LeetCode215. 数组中的第K个最大元素

--- LeetCodeJava/app/src/main/java/com/example/leetcodejava/Sort/LeetCode215.java
+++ LeetCodeJava/app/src/main/java/com/example/leetcodejava/Sort/LeetCode215.java
@@ -1,5 +1,8 @@
 package com.example.leetcodejava.Sort;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class LeetCode215 {
     /**
      *  LeetCode215. 数组中的第K个最大元素
@@ -20,4 +23,117 @@ public class LeetCode215 {
      *
      *  你可以假设 k 总是有效的，且 1 ≤ k ≤ 数组的长度。
      */
+
+    /**
+     * 方法一:排序
+     * 时间复杂度 O(NlogN)，空间复杂度 O(1)
+     */
+    public int findKtheLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+
+    /**
+     * 方法二、堆
+     * 时间复杂度 O(NlogK)，空间复杂度 O(K)。
+     */
+    public int findKtheLargest2(int[] nums, int k) {
+
+        return 1;
+    }
+
+    /**
+     * 方法三、基于快速排序的选择方法
+     * 时间复杂度 O(N)，空间复杂度 O(1)
+     */
+
+    Random random = new Random();
+
+    public int findKtheLargest3(int[] nums, int k) {
+
+        int index = nums.length - k;
+
+        return quickSelect(nums, 0, nums.length - 1, index);
+    }
+
+    /**
+     * 首先我们来回顾一下快速排序，这是一个典型的分治算法。我们对数组a[l⋯r] 做快速排序的过程是（参考《算法导论》）：
+     *
+     * 分解： 将数组 a[l⋯r] 「划分」成两个子数组a[l⋯q−1]、a[q+1⋯r]，使得 a[l⋯q−1] 中的每个元素小于等于a[q]，且a[q] 小于等于]a[q+1⋯r] 中的每个元素。其中，计算下标 q 也是「划分」过程的一部分。
+     * 解决： 通过递归调用快速排序，对子数组 a[l⋯q−1] a[q+1⋯r] 进行排序。
+     * 合并： 因为子数组都是原址排序的，所以不需要进行合并操作，a[l⋯r] 已经有序。
+     * 上文中提到的 「划分」 过程是：从子数组 a[l⋯r] 中选择任意一个元素 x 作为主元，调整子数组的元素使得左边的元素都小于等于它，右边的元素都大于等于它， xx的最终位置就是 q。
+     * 由此可以发现每次经过「划分」操作后，我们一定可以确定一个元素的最终位置，即 x的最终位置为 q，并且保证a[l⋯q−1] 中的每个元素小于等于a[q]，且a[q] 小于等于a[q+1⋯r] 中的每个元素。
+     * 所以只要某次划分的 q为倒数第 k个下标的时候，我们就已经找到了答案。 我们只关心这一点，至于a[l⋯q−1] 和 a[q+1⋯r] 是否是有序的，我们不关心。
+     *
+     * 借鉴快速排序的分治思想
+     * 因此我们可以改进快速排序算法来解决这个问题：在分解的过程当中，我们会对子数组进行划分，如果划分得到的 q 正好就是我们需要的下标，
+     * 就直接返回 a[q]a[q]；否则，如果 qq 比目标下标小，就递归右子区间，否则递归左子区间。
+     * 这样就可以把原来递归两个区间变成只递归一个区间，提高了时间效率。这就是「快速选择」算法。
+     */
+    public int quickSelect(int[] a, int left, int right, int index) {
+        int partition = randomPartition(a, left, right);
+
+        if (partition == index) {
+            return a[partition];
+        } else  {
+            return partition < index ? quickSelect(a, partition + 1, right, index) : quickSelect(a, left, partition - 1, index);
+        }
+    }
+
+    /**
+     *  为了应对极端测试用例，使得递归树加深，可以在循环一开始的时候，随机交换元素；
+     *  随机化切分元素
+     */
+    public int randomPartition(int[]a, int left, int right) {
+
+        int i = random.nextInt(right - left + 1) + left;
+
+        change(a, i, right);
+
+        return partition(a, left, right);
+    }
+
+    public void change(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    /**
+     * 将数组的某一段元素进行划分，小的在左边，大的在右边
+     */
+    public static int partition(int[] a, int start, int end){
+        //每次都以最右边的元素作为基准值
+        int base = a[end];
+        //start一旦等于end，就说明左右两个指针合并到了同一位置，可以结束此轮循环。
+        while(start < end){
+            while(start < end && a[start] <= base)
+                //从左边开始遍历，如果比基准值小，就继续向右走
+                start++;
+            //上面的while循环结束时，就说明当前的a[start]的值比基准值大，应与基准值进行交换
+            if(start < end){
+                //交换
+                int temp = a[start];
+                a[start] = a[end];
+                a[end] = temp;
+                //交换后，此时的那个被调换的值也同时调到了正确的位置(基准值右边)，因此右边也要同时向前移动一位
+                end--;
+            }
+            while(start < end && a[end] >= base)
+                //从右边开始遍历，如果比基准值大，就继续向左走
+                end--;
+            //上面的while循环结束时，就说明当前的a[end]的值比基准值小，应与基准值进行交换
+            if(start < end){
+                //交换
+                int temp = a[start];
+                a[start] = a[end];
+                a[end] = temp;
+                //交换后，此时的那个被调换的值也同时调到了正确的位置(基准值左边)，因此左边也要同时向后移动一位
+                start++;
+            }
+        }
+        //这里返回start或者end皆可，此时的start和end都为基准值所在的位置
+        return end;
+    }
 }
