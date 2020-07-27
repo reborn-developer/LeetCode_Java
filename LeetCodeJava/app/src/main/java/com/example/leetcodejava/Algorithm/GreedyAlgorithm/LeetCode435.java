bd93f7b82d85f85cbf6135f98281611b1eb2d720
reborn <343633674@qq.com>
2020-07-27 19:08:06 +0800

    LeetCode435. 无重叠区间(贪心算法)

--- LeetCodeJava/app/src/main/java/com/example/leetcodejava/Algorithm/GreedyAlgorithm/LeetCode435.java
+++ LeetCodeJava/app/src/main/java/com/example/leetcodejava/Algorithm/GreedyAlgorithm/LeetCode435.java
@@ -1,5 +1,8 @@
 package com.example.leetcodejava.Algorithm.GreedyAlgorithm;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class LeetCode435 {
     /**
      *  LeetCode435. 无重叠区间
@@ -25,4 +28,37 @@ public class LeetCode435 {
      *  输出: 0
      *  解释: 你不需要移除任何区间，因为它们已经是无重叠的了。
      */
+
+    /**
+     * 思路:
+     * 先计算最多能组成的不重叠区间个数，然后用区间总个数减去不重叠区间的个数。
+     *
+     * 在每次选择中，区间的结尾最为重要，选择的区间结尾越小，留给后面的区间的空间越大，那么后面能够选择的区间个数也就越大。
+     *
+     * 按区间的结尾进行排序，每次选择结尾最小，并且和前一个区间不重叠的区间。
+     */
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals.length == 0) {
+            return 0;
+        }
+//        Arrays.sort(intervals, Comparator.comparingInt(o -> o[1]));
+
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+
+        int cnt = 1;
+        int end = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < end) {
+                continue;
+            }
+            end = intervals[i][1];
+            cnt++;
+        }
+        return intervals.length - cnt;
+    }
 }
