f554829b153ad8b41da8baae904d1bbe7d84cc31
reborn <343633674@qq.com>
2020-07-29 14:54:36 +0800

    LeetCode122.买卖股票的最佳时机 II

--- LeetCodeJava/app/src/main/java/com/example/leetcodejava/Algorithm/GreedyAlgorithm/LeetCode122.java
+++ LeetCodeJava/app/src/main/java/com/example/leetcodejava/Algorithm/GreedyAlgorithm/LeetCode122.java
@@ -29,4 +29,20 @@ public class LeetCode122 {
      *  1 <= prices.length <= 3 * 10 ^ 4
      *  0 <= prices[i] <= 10 ^ 4
      */
+
+    /**
+     * 贪心算法
+     * 思路：
+     * 对于 [a, b, c, d]，如果有 a <= b <= c <= d ，那么最大收益为 d - a。而 d - a = (d - c) + (c - b) + (b - a) ，
+     * 因此当访问到一个 prices[i] 且 prices[i] - prices[i-1] > 0，那么就把 prices[i] - prices[i-1] 添加到收益中。
+     */
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += (prices[i] - prices[i - 1]);
+            }
+        }
+        return profit;
+    }
 }
