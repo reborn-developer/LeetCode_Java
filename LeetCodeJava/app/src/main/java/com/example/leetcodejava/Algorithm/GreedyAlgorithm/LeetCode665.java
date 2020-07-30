56e385e76f8940b3baeb08c0d9b295a87c726999
reborn <343633674@qq.com>
2020-07-30 11:05:46 +0800

    LeetCode665.非递减数列

--- LeetCodeJava/app/src/main/java/com/example/leetcodejava/Algorithm/GreedyAlgorithm/LeetCode665.java
+++ LeetCodeJava/app/src/main/java/com/example/leetcodejava/Algorithm/GreedyAlgorithm/LeetCode665.java
@@ -22,4 +22,27 @@ public class LeetCode665 {
      *  1 <= n <= 10 ^ 4
      *  - 10 ^ 5 <= nums[i] <= 10 ^ 5
      */
+
+    /**
+     *  贪心算法
+     *  思路
+     *  在出现 nums[i] < nums[i - 1] 时，需要考虑的是应该修改数组的哪个数，使得本次修改能使 i 之前的数组成为非递减数组，并且 不影响后续的操作 。
+     *  优先考虑令 nums[i - 1] = nums[i]，因为如果修改 nums[i] = nums[i - 1] 的话，那么 nums[i] 这个数会变大，就有可能比 nums[i + 1] 大，从而影响了后续操作。
+     *  还有一个比较特别的情况就是 nums[i] < nums[i - 2]，修改 nums[i - 1] = nums[i] 不能使数组成为非递减数组，只能修改 nums[i] = nums[i - 1]。
+     */
+    public boolean checkPossibility(int[] nums) {
+        int cnt = 0;
+        for (int i = 1; i < nums.length && cnt < 2; i++) {
+            if (nums[i] >= nums[i - 1]) {
+                continue;
+            }
+            cnt++;
+            if (i - 2 >= 0 && nums[i] < nums[i - 2]) {
+                nums[i] = nums[i - 1];
+            } else {
+                nums[i - 1] = nums[i];
+            }
+        }
+        return cnt <= 1;
+    }
 }
