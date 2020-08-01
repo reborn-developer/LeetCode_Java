f94bb96905000ca15b272233db6dde4ffff272e1
reborn <343633674@qq.com>
2020-08-01 22:24:29 +0800

    commit

--- LeetCodeJava/app/src/main/java/com/example/leetcodejava/Algorithm/GreedyAlgorithm/LeetCode406.java
+++ LeetCodeJava/app/src/main/java/com/example/leetcodejava/Algorithm/GreedyAlgorithm/LeetCode406.java
@@ -48,6 +48,9 @@ public class LeetCode406 {
      * 返回输出队列
      */
     public int[][] reconstructQueue(int[][] people) {
+        if (people == null || people.length == 0 || people[0].length == 0) {
+            return new int[0][0];
+        }
         Arrays.sort(people, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
